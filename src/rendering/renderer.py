cee959aad91c5edf810fc4fe38d9bb1acf8484e1
larissa-1101 <larissa.tay220203@gmail.com>
2026-04-18 22:31:14 +0200

    feat: add Panda3D red card test for teammate

--- src/rendering/renderer.py
+++ src/rendering/renderer.py
@@ -1,8 +1,16 @@
-class Renderer:
-    def __init__(self):
-        self.agent_nodes = []
+from direct.showbase.ShowBase import ShowBase
+from panda3d.core import CardMaker
 
-    def update(self, agents):
-        for i, agent in enumerate(agents):
-            # update node positions (Panda3D NodePath later)
-            pass
+class Renderer(ShowBase):
+    def __init__(self):
+        ShowBase.__init__(self)
+        cm = CardMaker("card")
+        cm.setFrame(-1, 1, -1, 1)
+        card = self.render.attachNewNode(cm.generate())
+        card.setColor(1, 0, 0, 1)
+        card.setTwoSided(True)
+        card.setLightOff()
+        self.camera.setPos(0, -3, 0)
+        self.camera.lookAt(0, 0, 0)
+
+Renderer().run()
